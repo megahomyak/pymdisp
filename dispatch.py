06b2b85fc61262f1dfd952153378cf90147ee65d
megahomyak <g.megahomyak@gmail.com>
2023-11-28 10:31:29 +0500

    Specified how unoptimized it is

--- dispatch.py
+++ dispatch.py
@@ -1,33 +1,39 @@
 import asyncio
+from functools import partial
 
-async def handler(waiter, msg):
-    print(msg)
-    print(await waiter)
-    print(await waiter)
+async def handler(key, waiter, msg):
+    print(key, msg)
+    print(key, await waiter)
+    print(key, await waiter)
 
 async def test():
     dispatcher = Dispatcher()
-    for key, message in [
+    tasks = []
+    for index, (key, message) in enumerate([
         (1, "a"),
         (2, "b"),
         (1, "c"),
         (2, "d"),
         (1, "e"),
         (2, "f"),
-    ]:
-        asyncio.create_task(
-            dispatcher.dispatch(
-                key, message, handler
+    ]):
+        async def task(number, key, message):
+            await asyncio.sleep((number ** 2) / 5)
+            await dispatcher.dispatch(
+                key, message, partial(handler, key)
             )
-        )
+        tasks.append(task(index + 1, key, message))
+    await asyncio.gather(*tasks)
 
 class Waiter:
 
     def __init__(self):
         self._queue = []
+        self._polls_amt = 0
 
     async def _wait(self):
         while True:
+            self._polls_amt += 1
             if self._queue:
                 item, *self._queue = self._queue
                 return item
@@ -50,6 +56,6 @@ class Dispatcher:
             await handler(waiter, message)
         else:
             waiter._queue.append(message)
+        print("Polls amount:", waiter._polls_amt, "(horrible, right?)")
 
-loop = asyncio.new_event_loop()
-loop.run_until_complete(test())
+asyncio.run(test())
